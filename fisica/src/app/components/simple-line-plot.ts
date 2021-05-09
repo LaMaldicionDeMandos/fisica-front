ecf5a8844b67b975a1cee7413b0e6ad05ea883b0
marcelo <pasutmarcelo@gmail.com>
2021-05-09 13:56:07 -0300

    implemento refresco del grafico cuando cambia data

--- fisica/src/app/components/simple-line-plot.ts
+++ fisica/src/app/components/simple-line-plot.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -6,13 +6,15 @@ import * as d3 from 'd3';
   templateUrl: './simple-line-plot.html',
   styleUrls: ['./simple-line-plot.scss']
 })
-export class SimpleLinePlotComponent implements OnInit {
+export class SimpleLinePlotComponent implements OnInit, OnChanges {
   @Input() title: string;
   @Input() data: any[];
   @Input() width = 700;
   @Input() height = 500;
   @Input() lineColor = 'blue';
   @Input() lineWeight = '1.5px';
+  @Input() horizontalLabel = 'x';
+  @Input() verticalLavel = 'y';
   private margin = 20;
   public svg;
   public svgInner;
@@ -24,12 +26,37 @@ export class SimpleLinePlotComponent implements OnInit {
 
   constructor(public chartElem: ElementRef) { }
 
+  ngOnChanges(changes: SimpleChanges) {
+    this.yScale.domain([
+        d3.max(this.data, d => d.y),
+        d3.min(this.data, d => d.y)
+      ])
+      .range([0, this.height - 2 * this.margin]);
+    this.xScale.domain(d3.extent(this.data, d => d.x));
+    this.xScale.range([this.margin, this.width - 2 * this.margin]);
+    const xAxis = d3
+      .axisBottom(this.xScale)
+      .ticks(this.width * 0.03);
+    this.xAxis.call(xAxis);
+    const yAxis = d3
+      .axisRight(this.yScale);
+    this.yAxis.call(yAxis);
+    const line = d3
+      .line()
+      .x(d => d[0])
+      .y(d => d[1])
+      .curve(d3.curveMonotoneX);
+    const points: [number, number][] = this.data.map(
+      d => [this.xScale(d.x), this.yScale(d.y)]
+    );
+    this.lineGroup.attr('d', line(points));
+  }
   ngOnInit() {
     this.svg = d3
       .select(this.chartElem.nativeElement)
       .select('.linechart')
       .append('svg')
-      .attr('height', this.height + this.margin);
+      .attr('height', parseFloat(String(this.height)) + parseFloat(String(this.margin)));
     this.svgInner = this.svg.append('g').style('transform', 'translate(' + this.margin + 'px, ' + this.margin + 'px)');
     this.yScale = d3.scaleLinear()
       .domain([
@@ -50,7 +77,7 @@ export class SimpleLinePlotComponent implements OnInit {
       .style('fill', 'none')
       .style('stroke', this.lineColor)
       .style('stroke-width', this.lineWeight);
-    this.svg.attr('width', this.width + this.margin);
+    this.svg.attr('width', parseFloat(String(this.width)) + parseFloat(String(this.margin)));
     this.xScale.range([this.margin, this.width - 2 * this.margin]);
     const xAxis = d3
       .axisBottom(this.xScale)
@@ -68,6 +95,20 @@ export class SimpleLinePlotComponent implements OnInit {
       d => [this.xScale(d.x), this.yScale(d.y)]
     );
     this.lineGroup.attr('d', line(points));
+    this.svg.append('text')
+      .attr('class', 'x label')
+      .attr('text-anchor', 'end')
+      .attr('x', parseFloat(String(this.width)))
+      .attr('y', parseFloat(String(this.height)) + 10)
+      .text(this.horizontalLabel);
+
+    this.svg.append('text')
+      .attr('class', 'y label')
+      .attr('text-anchor', 'end')
+      .attr('y', 15)
+      .attr('dy', '.75em')
+      .attr('transform', 'rotate(-90)')
+      .text(this.verticalLavel);
   }
 
 }
--- fisica/src/app/pages/cinematic/cinematic.page.ts
+++ fisica/src/app/pages/cinematic/cinematic.page.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {OperationBuilder} from '../../lib/prefix-functions';
+import * as _ from 'lodash';
 
 const MRU_X = '+ x0 * v tf';
 const MRU_T = '/ - xf x0 v';
 const MRU_V = '/ - xf x0 tf';
+
+const DEFAULT_STEPS = 2000;
 
 @Component({
   selector: 'app-cinematic-page',
@@ -17,16 +20,7 @@ export class CinematicPage implements OnInit {
   tf;
   xf;
   question;
-  data = [
-    {x: -1, y: -2},
-    {x: 0, y: 0},
-    {x: 1, y: 2},
-    {x: 2, y: 4},
-    {x: 3, y: 6},
-    {x: 4, y: 8},
-    {x: 5, y: 10},
-    {x: 6, y: 12},
-  ];
+  data = [];
 
   constructor() { }
 
@@ -34,29 +28,51 @@ export class CinematicPage implements OnInit {
 
   calculate() {
     this['calculate_' + this.question]();
+    this.data = this.generateData(MRU_X,
+      {x0: this.x0, v: this.v, tf: this.tf},
+      'tf',
+      {init: this.t0, end: this.tf}, DEFAULT_STEPS);
   }
 
   calculate_tf() {
-    this.tf = this.calculateFunction(MRU_T);
+    this.tf = this.calculateFunction(MRU_T, this);
     console.log(this.tf);
   }
 
   calculate_xf() {
-    this.xf = this.calculateFunction(MRU_X);
+    this.xf = this.calculateFunction(MRU_X, this);
     console.log(this.xf);
   }
 
   calculate_v() {
-    this.v = this.calculateFunction(MRU_V);
+    this.v = this.calculateFunction(MRU_V, this);
     console.log(this.v);
   }
 
-  private calculateFunction(func) {
-    return OperationBuilder.fromPrefixFunction(func).build().evaluate(this);
+  private calculateFunction(func, data) {
+    return OperationBuilder.fromPrefixFunction(func).build().evaluate(data);
   }
 
   private evaluate(result: OperationBuilder, value: string | number) {
     return result.addOperation(value);
+  }
+
+  private generateData(func, staticData, variableName, range, steps) {
+    const variables = this.createRangeArray(range.init, range.end, steps);
+    return _.map(variables, (variable) => {
+      staticData[variableName] = variable;
+      const result = this.calculateFunction(func, staticData);
+      return {x: variable, y: result};
+    });
+  }
+
+  private createRangeArray(init: number, end: number, steps: number) {
+    const delta = (end - init) / steps;
+    const array = [];
+    for (let point = init; point <= end; point += delta) {
+      array.push(point);
+    }
+    return array;
   }
 
 }
